b625bf22ed85255634b7650528e77f9ab476eb27
danielpiyo <danielopiyo8@gmail.com>
2023-08-28 14:25:09 +0300

    80% patient

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseURL: 'http://online.zyptech.co.ke:3000/api',
+  baseURL: 'http://66.228.48.247:3000/api',
   // mapskey: 'AIzaSyDQNKtSVa4islU0JGBDc0kiUm6ELCY5UsU',
   mapskey: 'AIzaSyBWArK1e0QiP_WLwoPhU4psoLQhmhegafI',
 };
